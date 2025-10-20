d9e0adebb0d827ec28b8e70e22bbda8b4f850a52
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-10-20 20:08:58 -0300

    feat: implement tabuada functionality and update HTML structure

--- javascript-curso/aula-14ex/ex017/script.js
+++ javascript-curso/aula-14ex/ex017/script.js
@@ -1,2 +1,12 @@
+function tabuada() {
+    const num = Number(document.getElementById('txti').value);
+    const tab = document.getElementById('tabuada');
+    tab.innerHTML = '';
 
+    for (let c=1; c<=10; c++) {
+       const i = document.createElement('option');
+       i.text = `${num} x ${c} = ${num*c}`
+       tab.appendChild(i);
+    }
+}
 
