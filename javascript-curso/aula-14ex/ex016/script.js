fe7c7aae6fbd5b4c97951040b44b0ad74eb4687f
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-10-20 08:46:06 -0300

    fix: rename function from 'contar' to 'count' and implement counting logic

--- javascript-curso/aula-14ex/ex016/script.js
+++ javascript-curso/aula-14ex/ex016/script.js
@@ -1,12 +1,33 @@
-function contar() {
-  const init = document.getElementById('txti') 
-  const end = document.getElementById('txtf')
-  const steps = document.getElementById('txtp')
-}
+function count() {
+   let i = document.getElementById('txti').value
+   let f = document.getElementById('txtf').value
+   let p = document.getElementById('txtp').value
+   let res = document.getElementById('res')
 
-if (init.value.lenght === 0 || end.value.lenght === 0 || steps.value.lenght === 0) {
-  window.alert('[ERRO] Verificar os dados para seguirmos com a contagem.')
-}else {
-  
-}
+   if (i.length == 0 || f.length == 0 || p.length == 0) {
+      window.alert('[ERRO] Verificar os dados para seguirmos com a contagem.')
+   } else {
+      res.innerHTML = 'Preparando contagem...'
+      let inicio = Number(i)
+      let fim = Number(f)
+      let passo = Number(p)
 
+      if(passo <= 0) {
+          window.alert('Passo inválido! Considerando PASSO 1')
+          passo = 1
+      }
+
+      if (inicio < fim) {
+        for(let c = inicio; c <= fim; c += passo) {
+          res.innerHTML += ` ${c} \u{1F449}`
+        }
+        res.innerHTML += ` \u{1F3C1}`
+      } else {
+        for(let c = inicio; c >= fim; c -= passo) {
+          res.innerHTML += ` ${c} \u{1F449}`
+        }
+        res.innerHTML += ` \u{1F3C1}`
+      }
+   }
+
+}
