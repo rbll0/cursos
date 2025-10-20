ed9be112ac4f839b1a8c74a43161684b01c89209
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-10-20 20:46:00 -0300

    fix: enhance tabuada function with input validation and correct option appending

--- javascript-curso/aula-14ex/ex017/script.js
+++ javascript-curso/aula-14ex/ex017/script.js
@@ -1,12 +1,14 @@
 function tabuada() {
-    const num = Number(document.getElementById('txti').value);
-    const tab = document.getElementById('tabuada');
-    tab.innerHTML = '';
+   const num = Number(document.getElementById('txti').value)
+   const tabua = document.getElementById('tabuada')
 
-    for (let c=1; c<=10; c++) {
-       const i = document.createElement('option');
-       i.text = `${num} x ${c} = ${num*c}`
-       tab.appendChild(i);
-    }
+   if ( num === 0 || Number.isNaN(num)) {
+     window.alert('Por favor digite um valor correto')
+   } else {
+     for( let c = 1; c <= 10; c++) {
+        const item = document.createElement('option')
+        item.text = `${num} x ${c} = ${num * c}`
+        item.appendChild(item)
+     }
+   }
 }
-
